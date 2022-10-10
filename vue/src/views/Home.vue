0a538233f9e64094502332bb2adad5bea768b9ee
‘qianxi’ <3112560244@qq.com>
2022-10-10 21:57:38 +0800

    完成home中的 饼状图，折现统计图

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -30,15 +30,32 @@ export default {
     var myChart = echarts.init(chartDom);
     var option;
 
+    // 饼图
+    var chartDom2 = document.getElementById('main2');
+    var myChart2 = echarts.init(chartDom2);
+    var option2;
 
     this.request.get("/echarts/memberDistribution").then(res =>{
       if (res.code === "200"){
-        option.xAxis .data= res.data.x
+        option.xAxis.data= res.data.x
         option.series[0].data = res.data.y
         option.series[1].data = res.data.y
 
+        let data = res.data
+        //设置名字
+        option2.series[0].data[0].name = data.x[0]
+        option2.series[0].data[1].name = data.x[1]
+        option2.series[0].data[2].name = data.x[2]
+        option2.series[0].data[3].name = data.x[3]
+
+        // 设置数值
+        option2.series[0].data[0].value = data.y[0]
+        option2.series[0].data[1].value = data.y[1]
+        option2.series[0].data[2].value = data.y[2]
+        option2.series[0].data[3].value = data.y[3]
 
         myChart.setOption(option);
+        myChart2.setOption(option2);
       }
     })
       option = {
@@ -61,134 +78,44 @@ export default {
       ]
     };
 
-    // var chartDom2 = document.getElementById('main2');
-    // var myChart2 = echarts.init(chartDom2);
-    // var option2;
 
-    // option2 = {
-    //   color: ['#67F9D8', '#FFE434', '#56A3F1', '#ff917c'],
-    //   title: {
-    //     text: 'Customized Radar Chart'
-    //   },
-    //   legend: {},
-    //   radar: [
-    //     {
-    //       indicator: [
-    //         { text: 'Indicator1' },
-    //         { text: 'Indicator2' },
-    //         { text: 'Indicator3' },
-    //         { text: 'Indicator4' },
-    //         { text: 'Indicator5' }
-    //       ],
-    //       center: ['25%', '50%'],
-    //       radius: 120,
-    //       startAngle: 90,
-    //       splitNumber: 4,
-    //       shape: 'circle',
-    //       axisName: {
-    //         formatter: '【{value}】',
-    //         color: '#428BD4'
-    //       },
-    //       splitArea: {
-    //         areaStyle: {
-    //           color: ['#77EADF', '#26C3BE', '#64AFE9', '#428BD4'],
-    //           shadowColor: 'rgba(0, 0, 0, 0.2)',
-    //           shadowBlur: 10
-    //         }
-    //       },
-    //       axisLine: {
-    //         lineStyle: {
-    //           color: 'rgba(211, 253, 250, 0.8)'
-    //         }
-    //       },
-    //       splitLine: {
-    //         lineStyle: {
-    //           color: 'rgba(211, 253, 250, 0.8)'
-    //         }
-    //       }
-    //     },
-    //     {
-    //       indicator: [
-    //         { text: 'Indicator1', max: 150 },
-    //         { text: 'Indicator2', max: 150 },
-    //         { text: 'Indicator3', max: 150 },
-    //         { text: 'Indicator4', max: 120 },
-    //         { text: 'Indicator5', max: 108 },
-    //         { text: 'Indicator6', max: 72 }
-    //       ],
-    //       center: ['75%', '50%'],
-    //       radius: 120,
-    //       axisName: {
-    //         color: '#fff',
-    //         backgroundColor: '#666',
-    //         borderRadius: 3,
-    //         padding: [3, 5]
-    //       }
-    //     }
-    //   ],
-    //   series: [
-    //     {
-    //       type: 'radar',
-    //       emphasis: {
-    //         lineStyle: {
-    //           width: 4
-    //         }
-    //       },
-    //       data: [
-    //         {
-    //           value: [100, 8, 0.4, -80, 2000],
-    //           name: 'Data A'
-    //         },
-    //         {
-    //           value: [60, 5, 0.3, -100, 1500],
-    //           name: 'Data B',
-    //           areaStyle: {
-    //             color: 'rgba(255, 228, 52, 0.6)'
-    //           }
-    //         }
-    //       ]
-    //     },
-    //     {
-    //       type: 'radar',
-    //       radarIndex: 1,
-    //       data: [
-    //         {
-    //           value: [120, 118, 130, 100, 99, 70],
-    //           name: 'Data C',
-    //           symbol: 'rect',
-    //           symbolSize: 12,
-    //           lineStyle: {
-    //             type: 'dashed'
-    //           },
-    //           label: {
-    //             show: true,
-    //             formatter: function (params) {
-    //               return params.value;
-    //             }
-    //           }
-    //         },
-    //         {
-    //           value: [100, 93, 50, 90, 70, 60],
-    //           name: 'Data D',
-    //           areaStyle: {
-    //             color: new echarts.graphic.RadialGradient(0.1, 0.6, 1, [
-    //               {
-    //                 color: 'rgba(255, 145, 124, 0.1)',
-    //                 offset: 0
-    //               },
-    //               {
-    //                 color: 'rgba(255, 145, 124, 0.9)',
-    //                 offset: 1
-    //               }
-    //             ])
-    //           }
-    //         }
-    //       ]
-    //     }
-    //   ]
-    // };
 
-    // myChart2.setOption(option2);
+    option2 = {
+      title: {
+        text: '季度新增用户统计',
+        subtext: '今年',
+        left: 'center'
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left'
+      },
+      series: [
+        {
+          name: 'Access From',
+          type: 'pie',
+          radius: '50%',
+          data: [
+            { value: 0, name: "" },
+            { value: 0, name: "" },
+            { value: 0, name: "" },
+            { value: 0, name: "" }
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    };
+
+
   }
 }
 </script>
